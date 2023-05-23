bae021e4f7a31da3179dd53a64353b3e05ab8dff
Bharath.S <bharaths0407@gmail.com>
2023-05-23 18:25:43 +0530

    Splash Screen

--- front-end/app/src/main/java/com/example/buzzhub/MainActivity.java
+++ front-end/app/src/main/java/com/example/buzzhub/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.buzzhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent ilanding = new Intent(MainActivity.this, landingActivity.class);
+                startActivity(ilanding);
+                finish();
+            }
+        }, 5000);
     }
 }
---
+++ front-end/app/src/main/java/com/example/buzzhub/landingActivity.java
@@ -0,0 +1,14 @@
+package com.example.buzzhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class landingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
