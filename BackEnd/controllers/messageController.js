7da90f2568256fd721dfb86f2192288d38e60a5a
rohith02b <rohith20b@gmail.com>
2023-06-02 16:42:47 +0530

    Messaging controller done

--- BackEnd/controllers/messageController.js
+++ BackEnd/controllers/messageController.js
@@ -1,11 +1,28 @@
 const Message = require('../models/Messages');
 
 const postMessage = async (req, res) => {
-  return res.json({ message: 'Hi' });
+  const newMessage = new Message({
+    conversationId: req.body.conversationId,
+    sender: req.id,
+    text: req.body.text,
+  });
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const getMessage = async (req, res) => {
-  return res.json({ message: 'Hi' });
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.id,
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 module.exports = { postMessage, getMessage };
--- BackEnd/user_routes/messageRoutes.js
+++ BackEnd/user_routes/messageRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const isLoggedIn = require('../middleware/isLoggedIn');
 const { postMessage, getMessage } = require('../controllers/messageController');
 
-router.get('/', isLoggedIn, getMessage);
+router.get('/:id', isLoggedIn, getMessage);
 router.post('/', isLoggedIn, postMessage);
 
 module.exports = router;
