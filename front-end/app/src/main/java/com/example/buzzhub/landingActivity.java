b76c3ecfa300c098cd37fb0eb92dd1a634ba3593
Bharath.S <bharaths0407@gmail.com>
2023-06-13 10:08:26 +0530

    Bottom navi

--- front-end/app/src/main/java/com/example/buzzhub/landingActivity.java
+++ front-end/app/src/main/java/com/example/buzzhub/landingActivity.java
@@ -30,7 +30,7 @@ public class landingActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent log = new Intent(landingActivity.this, loginpageActivity.class);
+                Intent log = new Intent(landingActivity.this, homepageActivity.class);
                 startActivity(log);
                 finish();
             }
